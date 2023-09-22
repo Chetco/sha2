0854ab3bb66afdcc4f912916ac112b2b310bcca8
chetco <none>
2023-09-22 08:07:34 -0700

    Thanks to Mārtiņš Možeiko for some suggestions the API returns a byte array now. A bug is fixed with compilers using a 32 bit size_t which did not work for input greater than 512MB.

--- chet_sha256.h
+++ chet_sha256.h
@@ -1,22 +1,12 @@
 /*
-chet_sha256 - v1.0.0 - public domain sha2
+chet_sha256 - v2.0.0 - public domain sha2
     no warranty implied; use at your own risk
-    supports c99 or later
+    supports c99 or later and should work with C++
 
-USAGE
-    Do this:
-    #define CHET_SHA256_IMPLEMENTATION
-    before you include this file in *one* C or C++ file to create the implementation.
-
-    // i.e. it should look like this:
-    #include ...
-    #include ...
-    #include ...
-    #define CHET_SHA256_IMPLEMENTATION
-    #include "stb_image.h"
+    api calls begin with 'chet_' and please ignore identifiers beginning with 'ichet_' which are internal
 
 API
-    void chet_sha256(const void *input, size_t byte_length, uint32_t hash_result[8])
+    void chet_sha256(const void *input, size_t byte_length, uint8_t hash_result[32])
 
 LICENSE
     See the end of this file for the license information.
@@ -25,57 +15,52 @@ LICENSE
 #ifndef CHET_SHA2__SHA256_H_INCLUDED
 #define CHET_SHA2__SHA256_H_INCLUDED
 
-#ifdef __cplusplus
-#include <cstdint>
-#else
 #include <stdint.h>
-#endif
+#include <stddef.h>
+#include <string.h>
 
-/* BEGIN API */
+static inline uint64_t ichet_sha2__swap_endian_64(uint64_t x)
+{
+    x = (x & 0x00000000FFFFFFFF) << 32 | (x & 0xFFFFFFFF00000000) >> 32;
+    x = (x & 0x0000FFFF0000FFFF) << 16 | (x & 0xFFFF0000FFFF0000) >> 16;
+    x = (x & 0x00FF00FF00FF00FF) << 8  | (x & 0xFF00FF00FF00FF00) >> 8;
+    return x;
+}
+
+static inline uint64_t ichet_sha2__div_round_up(uint64_t value, size_t align)
+{
+    return (value + align - 1) / align;
+}
+
+static inline void ichet_sha2__pack_u32(uint32_t src, uint8_t *dest)
+{
+    dest[0] = (src & 0xFF000000) >> 24;
+    dest[1] = (src & 0x00FF0000) >> 16;
+    dest[2] = (src & 0x0000FF00) >>  8;
+    dest[3] = (src & 0x000000FF);
+}
+
+#define ichet_sha2__choose(x, y, z) ((x & y) ^ ((~x) & z))
+#define ichet_sha2__majority(x, y, z) ((x & y) ^ (x & z) ^ (y & z))
+#define ichet_sha2__rotr(x, n) ((x >> n) | (x << (32 - n)))
+#define ichet_sha2__SIGMA_0(x) (ichet_sha2__rotr(x, 2) ^ ichet_sha2__rotr(x, 13) ^ ichet_sha2__rotr(x, 22))
+#define ichet_sha2__SIGMA_1(x) (ichet_sha2__rotr(x, 6) ^ ichet_sha2__rotr(x, 11) ^ ichet_sha2__rotr(x, 25))
+#define ichet_sha2__sigma_0(x) (ichet_sha2__rotr(x, 7) ^ ichet_sha2__rotr(x, 18) ^ (x >> 3))
+#define ichet_sha2__sigma_1(x) (ichet_sha2__rotr(x, 17) ^ ichet_sha2__rotr(x, 19) ^ (x >> 10))
 
 /* `input` will have `byte_length` bytes hashed, this doesn't hash bit lengths */
-extern void chet_sha256(const void *input, size_t byte_length, uint32_t hash_result[8]);
-
-/* END API */
-
-#endif
-
-#ifdef CHET_SHA256_IMPLEMENTATION
-
-#ifdef __cplusplus
-#include <cstring>
-#else
-#include <string.h>
-#endif
-
-static uint64_t chet_sha2__swap_endian_64(uint64_t x)
+static inline void chet_sha256(const void *input, size_t byte_length, uint8_t hash_result[32])
 {
-    return (x & ((uint64_t)0xFF      )) << 56 |
-           (x & ((uint64_t)0xFF << 8 )) << 40 |
-           (x & ((uint64_t)0xFF << 16)) << 24 |
-           (x & ((uint64_t)0xFF << 24)) <<  8 |
-           (x & ((uint64_t)0xFF << 32)) >>  8 |
-           (x & ((uint64_t)0xFF << 40)) >> 24 |
-           (x & ((uint64_t)0xFF << 48)) >> 40 |
-           (x & ((uint64_t)0xFF << 56)) >> 56;
-}
+    uint32_t H0 = 0x6a09e667;
+    uint32_t H1 = 0xbb67ae85;
+    uint32_t H2 = 0x3c6ef372;
+    uint32_t H3 = 0xa54ff53a;
+    uint32_t H4 = 0x510e527f;
+    uint32_t H5 = 0x9b05688c;
+    uint32_t H6 = 0x1f83d9ab;
+    uint32_t H7 = 0x5be0cd19;
 
-static size_t chet_sha2__boundary_ceil(size_t value, size_t align)
-{
-    return (value + align - 1) / align * align;
-}
-
-#define chet_sha2__choose(x, y, z) ((x & y) ^ ((~x) & z))
-#define chet_sha2__majority(x, y, z) ((x & y) ^ (x & z) ^ (y & z))
-#define chet_sha2__rotr(x, n) ((x >> n) | (x << (32 - n)))
-#define chet_sha2__SIGMA_0(x) (chet_sha2__rotr(x, 2) ^ chet_sha2__rotr(x, 13) ^ chet_sha2__rotr(x, 22))
-#define chet_sha2__SIGMA_1(x) (chet_sha2__rotr(x, 6) ^ chet_sha2__rotr(x, 11) ^ chet_sha2__rotr(x, 25))
-#define chet_sha2__sigma_0(x) (chet_sha2__rotr(x, 7) ^ chet_sha2__rotr(x, 18) ^ (x >> 3))
-#define chet_sha2__sigma_1(x) (chet_sha2__rotr(x, 17) ^ chet_sha2__rotr(x, 19) ^ (x >> 10))
-
-void chet_sha256(const void *input, size_t byte_length, uint32_t hash_result[8])
-{
-    uint32_t k_const[64] =
+    static const uint32_t k_const[64] =
     {
         0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
         0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
@@ -87,14 +72,16 @@ void chet_sha256(const void *input, size_t byte_length, uint32_t hash_result[8])
         0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
     };
 
-    uint8_t final_chunks[128] = {0}; /* space for two chunks in case two are needed if padding will cross boundary */
-    uint8_t *data = (uint8_t *)input;
+    /* create space for two chunks to handle the case where the padding size exceeds the 512 bit boundary */
+    uint8_t final_chunks[128] = {0};
+    const uint8_t *data = (const uint8_t *)input;
 
-    size_t bit_length = byte_length * 8;
+    /* explicitly use a 64 bit type in case of overflow on machines that use 32 bits for size_t */
+    uint64_t bit_length = (uint64_t)byte_length * 8;
     size_t full_chunks = bit_length / 512;
 
     /* determine the total number of chunks considering extra 1 bit and 64 bits for length. */
-    size_t total_chunks = chet_sha2__boundary_ceil(bit_length + 1 + 64, 512) / 512;
+    size_t total_chunks = ichet_sha2__div_round_up(bit_length + 1 + 64, 512);
     size_t number_final_chunks = total_chunks - full_chunks;
 
     size_t bit_remainder = bit_length % 512;
@@ -105,21 +92,12 @@ void chet_sha256(const void *input, size_t byte_length, uint32_t hash_result[8])
     uint32_t w_schedule[64] = {0};
     size_t chunk = 0;
 
-    uint64_t encoded_length = chet_sha2__swap_endian_64(bit_length);
+    uint64_t encoded_length = ichet_sha2__swap_endian_64(bit_length);
 
     /* create the final chunk / padded chunk */
     memcpy(&final_chunks[0], &data[full_chunks * 64], bit_remainder / 8);
     final_chunks[final_chunks_byte_length - padding_bytes] = 1 << 7; /* append a byte disguised as a single bit */
     memcpy(&final_chunks[final_chunks_byte_length - sizeof(encoded_length)], &encoded_length, sizeof(encoded_length));
-
-    hash_result[0] = 0x6a09e667;
-    hash_result[1] = 0xbb67ae85;
-    hash_result[2] = 0x3c6ef372;
-    hash_result[3] = 0xa54ff53a;
-    hash_result[4] = 0x510e527f;
-    hash_result[5] = 0x9b05688c;
-    hash_result[6] = 0x1f83d9ab;
-    hash_result[7] = 0x5be0cd19;
 
     while (chunk < (full_chunks + number_final_chunks))
     {
@@ -129,7 +107,7 @@ void chet_sha256(const void *input, size_t byte_length, uint32_t hash_result[8])
         /* detect if we are processing the last (padded) chunk(s) */
         if (chunk == full_chunks)
         {
-            data = final_chunks; /* switch to processing final_chunks */
+            data = (const uint8_t *)final_chunks; /* switch to processing final_chunks */
         }
 
         /* 1. */
@@ -142,25 +120,25 @@ void chet_sha256(const void *input, size_t byte_length, uint32_t hash_result[8])
         }
         for (word = 16; word < 64; ++word)
         {
-            w_schedule[word] = chet_sha2__sigma_1(w_schedule[word - 2]) + w_schedule[word - 7] +
-                chet_sha2__sigma_0(w_schedule[word - 15]) + w_schedule[word - 16];
+            w_schedule[word] = ichet_sha2__sigma_1(w_schedule[word - 2]) + w_schedule[word - 7] +
+                ichet_sha2__sigma_0(w_schedule[word - 15]) + w_schedule[word - 16];
         }
 
         /* 2. */
-        uint32_t a = hash_result[0];
-        uint32_t b = hash_result[1];
-        uint32_t c = hash_result[2];
-        uint32_t d = hash_result[3];
-        uint32_t e = hash_result[4];
-        uint32_t f = hash_result[5];
-        uint32_t g = hash_result[6];
-        uint32_t h = hash_result[7];
+        uint32_t a = H0;
+        uint32_t b = H1;
+        uint32_t c = H2;
+        uint32_t d = H3;
+        uint32_t e = H4;
+        uint32_t f = H5;
+        uint32_t g = H6;
+        uint32_t h = H7;
 
         /* 3. */
         for (tdx = 0; tdx < 64; ++tdx)
         {
-            uint32_t T1 = h + chet_sha2__SIGMA_1(e) + chet_sha2__choose(e, f, g) + k_const[tdx] + w_schedule[tdx];
-            uint32_t T2 = chet_sha2__SIGMA_0(a) + chet_sha2__majority(a, b, c);
+            uint32_t T1 = h + ichet_sha2__SIGMA_1(e) + ichet_sha2__choose(e, f, g) + k_const[tdx] + w_schedule[tdx];
+            uint32_t T2 = ichet_sha2__SIGMA_0(a) + ichet_sha2__majority(a, b, c);
             h = g;
             g = f;
             f = e;
@@ -172,18 +150,27 @@ void chet_sha256(const void *input, size_t byte_length, uint32_t hash_result[8])
         }
 
         /* 4. */
-        hash_result[0] += a;
-        hash_result[1] += b;
-        hash_result[2] += c;
-        hash_result[3] += d;
-        hash_result[4] += e;
-        hash_result[5] += f;
-        hash_result[6] += g;
-        hash_result[7] += h;
+        H0 += a;
+        H1 += b;
+        H2 += c;
+        H3 += d;
+        H4 += e;
+        H5 += f;
+        H6 += g;
+        H7 += h;
 
         data += 64;
         chunk++;
     }
+
+    ichet_sha2__pack_u32(H0, hash_result); hash_result += 4;
+    ichet_sha2__pack_u32(H1, hash_result); hash_result += 4;
+    ichet_sha2__pack_u32(H2, hash_result); hash_result += 4;
+    ichet_sha2__pack_u32(H3, hash_result); hash_result += 4;
+    ichet_sha2__pack_u32(H4, hash_result); hash_result += 4;
+    ichet_sha2__pack_u32(H5, hash_result); hash_result += 4;
+    ichet_sha2__pack_u32(H6, hash_result); hash_result += 4;
+    ichet_sha2__pack_u32(H7, hash_result);
 }
 
 #endif
